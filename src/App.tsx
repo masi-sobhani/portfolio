ef863668ef63d043362d00ad05c3aa542e4dcbbc
DOO-DEV <nimo.vimo.yank@gmail.com>
2025-08-04 19:12:22 +0330

    feat: change router to hash router for browser suppprt

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Navigation from './components/Navigation';
 import LoadingSpinner from './components/LoadingSpinner';
